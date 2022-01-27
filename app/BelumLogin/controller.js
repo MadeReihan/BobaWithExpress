f9b4e67bb4d6523d52eda33d1b8f68e176778d6c
MadeReihan <reihanmade@gmail.com>
2022-01-27 23:41:13 +0700

    ~

--- app/BelumLogin/controller.js
+++ app/BelumLogin/controller.js
@@ -3,12 +3,17 @@ module.exports={
     index: async(req,res)=>{
         try {
             const facility = await Facility.find()
-
+            const alertMessage = req.flash("alertMessage")
+            const alertStatus = req.flash("alertStatus")
+            const alert = {message:alertMessage, status:alertStatus}
             res.render('index',{
                 session:req.session.user,
-                facility
+                facility,
+                alert
             })
         } catch (err) {
+            req.flash('alertMessage',`${err.message}`)
+            req.flash('alertStatus', 'error')
             console.log(err);
         }
     }
